a4e8ddb009914bed87feabd26f4ff8c997e30b2f
Hariomsingh98 <hariomjawadi100@gmail.com>
2023-02-25 23:49:08 +0530

    done

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class OrderServices {
+public class OrderService {
     @Autowired
     OrderRepository orderRepository;
 
